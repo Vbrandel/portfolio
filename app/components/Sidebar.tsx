847c065b646351c15305f9d9eb327c0682d8b1f5
Brandel <v.brandel76@gmail.com>
2025-09-28 19:08:43 +0200

    ajout CV target blank

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -5,7 +5,7 @@ const Sidebar = () => {
     <div className="text-white bg-stone-800 h-screen p-6 flex flex-col gap-6 md:gap-6 2xl:gap-12">
       <div className="flex justify-between">
         <span className="inline-block px-4 py-2 bg-green-100 text-green-800 rounded-full text-sm font-medium size-fit">Open to work</span>
-        <a href="#" className="px-4 py-2 border border-gray-500 rounded-full size-fit text-sm hover:bg-gray-700 scroll-smooth"><i className="fa-solid fa-file mr-2"></i>Mon CV</a>
+        <a href="assets/cv.pdf" target="_blank" className="px-4 py-2 border border-gray-500 rounded-full size-fit text-sm hover:bg-gray-700 scroll-smooth"><i className="fa-solid fa-file mr-2"></i>Mon CV</a>
       </div>
       <img src="/assets/tete.png" className="rounded-full md:w-50 2xl:w-60 mx-auto bg-stone-200"></img>
       <div className="text-center text-stone-200">
